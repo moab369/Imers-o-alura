bcfafa94d2782e83044e96346b0926464dc8fe24
Lucas Gabriel moab da silva lima <133785140+moab369@users.noreply.github.com>
2025-01-26 20:52:10 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 function botao() {
     // Seleciona o elemento HTML onde os resultados serão exibidos
     let resultado = document.getElementById('resultados-pesquisa');
-
+    let pesquisa = document.getElementById('pesquisa').value;
+    if(pesquisa == ""){
+      resultado.innerHTML = "<p>Nemhum resultado encontrado</p>"
+     return;
+    }
+    pesquisa = pesquisa.toLowerCase()
     // Inicializa uma string vazia para armazenar o HTML dos resultados
     let texto = "";
+    let titulo = "";
+    let descicao = "";
+    
 
     // Itera sobre os dados da pesquisa e gera o HTML para cada resultado
     for (let dado of dados) {
+      titulo = dado.titulo.toLowerCase();
+      descicao = dado.descicao.toLowerCase();
+      if(titulo.includes(pesquisa) || descicao.includes(pesquisa)){
         texto += `
             <div class="item-resultado">
                 <h2>${dado.titulo}</h2>
@@ -14,8 +25,12 @@ function botao() {
                 <p><a href="${dado.link}" target="_blank">Saiba mais</a></p> 
             </div>
         `;
+      }
     }
-
+   if(!resultado){
+     resultado.innerHTML = "<p>Nemhum resultado encontrado</p>"
+   }
     // Atualiza o conteúdo do elemento HTML com os resultados gerados
     resultado.innerHTML = texto;
+    resultado = "";
 }
